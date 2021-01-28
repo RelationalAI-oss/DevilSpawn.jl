10c7e9d7bcd15205ac34957262103a4bf5db28e5
Nathan Daly <44379820+rai-nhdaly@users.noreply.github.com>
2021-01-28 12:51:51 -0500

    Add comment explaining the sleep.

--- src/DevilSpawn.jl
+++ src/DevilSpawn.jl
@@ -20,6 +20,11 @@ end
 
 function spawn_background(f, attempt_num=0)
     if Threads.threadid() == 1 && Threads.nthreads() > 1
+        # Out of concern that we might build up a bunch of tasks, all clamoring to schedule
+        # on main while we wait for an availabl thread, we use a very small exponential
+        # backoff between scheduling attempts. We keep this very fast, so that it shouldn't
+        # contribute noticable latency, but will help to prevent many enqueued tasks from
+        # burning CPU while they wait.
         sleep(1E-9 * (1 << min(32, attempt_num)))
         # Maintain demand on the new Task, so that errors will propagate.
         return fetch(Threads.@spawn spawn_background(f, attempt_num+1))
